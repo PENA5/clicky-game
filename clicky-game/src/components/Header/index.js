2bf25eb98a2c48bb4fe5c91f638bb8bebc7aab18
PENA5 <44989115+PENA5@users.noreply.github.com>
2019-04-27 05:51:48 -0500

    src/cards.json

--- clicky-game/src/components/Header/index.js
+++ clicky-game/src/components/Header/index.js
@@ -7,6 +7,10 @@ const Header = props => (
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
+    <div className="banner">
+    <h1>Game of Thrones Edition</h1>
+    <p>Click on an image to earn points, but don't click on any more than once!</p>
+    </div>
   </div>
 );
 
